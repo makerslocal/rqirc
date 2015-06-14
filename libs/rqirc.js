17af2b12e8ebf8eedab1bebee0c9473a0a7f6d4e
Nathan W <nathan@frcv.net>
2015-06-13 22:18:44 -0500

    Validate some of the data we get in message

--- libs/rqirc.js
+++ libs/rqirc.js
@@ -10,6 +10,18 @@ var couch   = new Couch(config.couch);
 var Irc     = require('./irc.js');
 var irc     = new Irc(config.irc);
 
+
+function validateData(doc){
+  // Check if sender of message is also receaver
+  if ( doc.sender === config.rq.sender ) { throw 'Sender is me'; }
+
+  // Chck the whitelist
+  if ( config.irc.opts.channels.indexOf(doc.data.channel) === -1) { throw "Channel not in whitelist"; }
+
+  return true;
+}
+
+
 // Watch couch for a doc changee
 couch.feed.on('change', function (change) {
   var doc = change.doc;
@@ -18,11 +30,15 @@ couch.feed.on('change', function (change) {
   // Send all messages to ##rqtest
   irc.debugSend(doc);
 
-  //validateData(doc);
-  if ( doc.destination === config.rq.sender ){
+  try {
+    validateData(doc);
     log.info("Sending irc - [to: %s]",doc.data.channel);
     irc.send(doc.data.channel, doc.data.message, doc.data.isaction);
   }
+  catch (error) {
+    log.error("%s",error);
+  }
+
 });
 
 couch.feed.on('error', function(er) {
