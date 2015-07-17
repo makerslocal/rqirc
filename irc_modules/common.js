d1af744624a5fda93145ed70a0e690c9f6a2f662
Nathan W <nathan@frcv.net>
2015-07-17 16:08:10 -0500

    hack job for alert light

--- irc_modules/common.js
+++ irc_modules/common.js
@@ -1,4 +1,5 @@
 
 module.exports = function(irc, couch) {
-  require('../irc_modules/echo.js')(irc);
+  require('../irc_modules/echo.js')(irc,couch);
+  require('../irc_modules/alert.js')(irc,couch);
 };
