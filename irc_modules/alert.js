d08313a9b8be03b513f51db8e4ab56254c62ed88
Nathan W <nathan@frcv.net>
2015-08-18 22:23:03 -0500

    adding light and sound options to alert light

--- irc_modules/alert.js
+++ irc_modules/alert.js
@@ -1,9 +1,30 @@
-var log  = require('logule').init(module, 'echo.js');
+var util = require('util');
+var log  = require('logule').init(module, 'alert.js');
 module.exports = function(irc, redqueen) {
   irc.rqevent.on('alert', function(msg){
-    log.info('%s %s', msg.reply, msg.text);
-    var data = {text: msg.text, nolight: false, sender: msg.nick, nosound: false};
+    log.info('RECEIVED MSG: %s %s', msg.reply, msg.text);
+    var str = msg.text;
+
+    // look for nolight
+    var relight = /--nolight\s*/;
+    var light = true;
+    if (str.match(relight)) {
+      str = str.replace(relight, '');
+      light = false;
+    }
+
+    // look for nosound
+    var resound = /--nosound\s*/;
+    var sound = true;
+    if (str.match(resound)) {
+      str = str.replace(resound, '');
+      sound = false;
+    }
+
+    var data = {text: str, light: light, sender: msg.nick, sound: sound};
     redqueen.send('command', 'bigsign', data);
-    irc.send(msg.reply, 'Alerting sign!', false);
+    var reply = util.format('%s: New diplomatic cable decrypted: %s', msg.nick, str);
+    irc.send(msg.reply, reply, false);
+    log.info('SENDING MSG: %s %s', msg.reply, reply);
   });
 };
