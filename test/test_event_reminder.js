bd28ecc090136b4287c2875641680bfcfcc113ff
Nathan W <nathan@frcv.net>
2016-07-06 00:41:28 -0500

    fixed tests

--- test/test_event_reminder.js
+++ test/test_event_reminder.js
@@ -1,6 +1,7 @@
 var should = require('chai').should();
 var rewire = require('rewire');
 var event_reminder = rewire('../irc_modules/event_reminder');
+var moment = require('moment-timezone');
 
 var log = event_reminder.__get__('log');
 log.unmuteOnly(); // unmuteOnly nothing === mute everything
@@ -11,7 +12,8 @@ describe('event_reminder.mkMessage', function() {
   var mkMessage = event_reminder.__get__('mkMessage');
 
   it('should generate a message', function() {
+    var time = moment().calendar();
     var testData = {"type":"VEVENT","params":[],"start":"2016-07-06T23:00:00.000Z","end":"2016-07-07T00:00:00.000Z","uid":"77IK4NmWQsTfH5zx+otc1g","summary":"Leather Bracer Finishing Session","url":"http://ml256.org/w35n","description":"http://ml256.org/w35n","msg":"week"}
-    mkMessage(testData).should.equal('Remember: Leather Bracer Finishing Session is happening Tomorrow at 6:00 PM http://ml256.org/w35n');
+    mkMessage(testData).should.match(/^Remember: Leather Bracer Finishing Session is happening .* http:\/\/ml256.org\/w35n/);
   });
 })
